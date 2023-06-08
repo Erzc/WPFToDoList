4396c71541006cff9b4daa4bd0ec36c7c73c1efe
Erzc <84688309+Erzc@users.noreply.github.com>
2023-06-08 17:41:30 -0600

    If else statement to determine goal date length

--- To Do List/ModifyItemsForm.xaml.cs
+++ To Do List/ModifyItemsForm.xaml.cs
@@ -21,6 +21,10 @@ namespace To_Do_List
     /// </summary>
     public partial class ModifyItemsForm : Window
     {
+        DateTime userDT = new DateTime(2023, 1, 20, 12, 30, 0);
+
+
+
         public ModifyItemsForm()
         {
             InitializeComponent();
@@ -34,6 +38,25 @@ namespace To_Do_List
             string title = titleTb.Text; ;
             string description = descriptionTb.Text;
 
+            DateTime currentDT = DateTime.Now;
+
+            if (userDT <= currentDT)
+            {
+                DialogResult = false;
+            }
+            //Short term goal is less than 1 year from the current time
+            else if ((userDT.Year + currentDT.Year) < 1)
+            {
+                DialogResult = true;
+            }
+            //Long term goal is greater than 1 year
+            else
+            {
+                DialogResult = true;
+            }
+
+
+
             Close();
         }
 
@@ -45,8 +68,8 @@ namespace To_Do_List
 
         private void dateChanged(object sender, SelectionChangedEventArgs e)
         {
-            DateTime userDate = dueDateCalendar.SelectedDate ?? DateTime.MinValue;
-            Console.WriteLine($"User selected date: {userDate.ToShortDateString()}");
+            userDT = dueDateCalendar.SelectedDate ?? DateTime.MinValue;
+            Console.WriteLine($"User selected date: {userDT.ToShortDateString()}");
         }
     }
 }
